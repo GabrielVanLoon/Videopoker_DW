3619d6ca7f00fec955dbc8f86b043e650678272f
Albert Wolf <Albert Wolf@DESKTOP-6LRQFGO>
2019-05-15 22:15:38 -0300

    TelaJogo atualizada

--- aula12_PokerGui/VideoPoker/src/views/TelaJogo.java
+++ aula12_PokerGui/VideoPoker/src/views/TelaJogo.java
@@ -1,9 +1,11 @@
 package views;
 
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
@@ -31,14 +33,34 @@ public class TelaJogo extends JPanel {
 		layers.setLayer(background, 1);
 		add(background);
 		
-		/**
-		 * Adicionado Botão Jogar
-		 */
-		JButton btnJogar = new JButton("NOVO JOGO");
-		btnJogar.setBounds(550, 300, 200, 40);
-		layers.setLayer(btnJogar, 2);
-		add(btnJogar);
+		JPanel jp = new JPanel();
+		jp.setLayout(new GridLayout(3, 1));
 		
+		//Aposta que aparece na parte superior da tela
+		JPanel aposta = new JPanel();
+		aposta.setLayout(new GridLayout(1, 2));
+		aposta.add(new JLabel ("VALOR DA APOSTA: "));
+		aposta.add(new JLabel("XXX")); //Valor da aposta atual escolhida pelo usuario
+		
+		//Cartas atualmente na mesa
+		JPanel cartas = new JPanel();
+		cartas.setLayout(new GridLayout(1, 5));
+		cartas.add(new JLabel(new ImageIcon(".\\resources\\deck\\13_copas.jpg")));
+		cartas.add(new JLabel(new ImageIcon(".\\resources\\deck\\4_espadas.jpg")));
+		cartas.add(new JLabel(new ImageIcon(".\\resources\\deck\\5_espadas.jpg")));
+		cartas.add(new JLabel(new ImageIcon(".\\resources\\deck\\6_ouros.jpg")));
+		cartas.add(new JLabel(new ImageIcon(".\\resources\\deck\\8_copas.jpg")));
+		
+		//Botoes para trocar ou nao cartas
+		JPanel botoes = new JPanel();
+		botoes.setLayout(new GridLayout (1, 2));
+		botoes.add(new JLabel("TROCAR CARTA(S)"));
+		botoes.add(new JLabel("MANTER CARTAS"));
+		
+		jp.add(aposta);
+		jp.add(cartas);
+		jp.add(botoes);
+		
+		add(jp);
 	}
-	
 }
